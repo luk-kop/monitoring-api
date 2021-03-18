1a2e680d12f56b6f8be96a69e5393bc58e4cb67e
luk-kop <luk.kopicki@gmail.com>
2021-03-18 23:03:46 +0100

    Check service status in dedicated thread.

--- watchdog.py
+++ watchdog.py
@@ -5,6 +5,8 @@ import time
 import socket
 import logging
 from pymongo import MongoClient
+import threading
+from uuid import uuid4
 
 
 class MonitoringService:
@@ -16,51 +18,63 @@ class MonitoringService:
         self.services_collection = self.db.services
 
     def start(self):
+        """
+        Starts a watchdog service. Each service is being tested in a separate thread.
+        """
         while True:
             timer_start = time.perf_counter()
             services = self.get_services()
             if services:
                 for service in services:
-                    # TODO: check ip and continue if exception
-                    host = service['host']
-                    port = service['port']
-                    service_to_update = {'name': service['name']}                       # TODO: validate unique service name
-                    if service['proto'] == 'tcp':
-                        # timeout 1s (w 1)
-                        response = os.system(f'nc -z -w 1 {host} {port}')
-                    else:
-                        response = os.system(f'nc -zu {host} {port}')
-                    if response == 0:
-                        print(f'{service["name"]} is UP!')
-                        # Mark service as responded
-                        response_new_value = {'$set': {'last_responded': datetime.utcnow()}}
-                        self.services_collection.update_one(service_to_update, response_new_value)
-                        service['last_responded'] = datetime.utcnow()
-                        service_status = True
-                    else:
-                        print(f'{service["name"]} is DOWN')
-                        service_status = False
-                    # Update service in db only if 'status' is changed
-                    if service_status != service['service_up']:
-                        status_new_value = {'$set': {'service_up': service_status}}
-                        print(self.services_collection.find_one(service_to_update))        # TODO: to delete
-                        # Update service status
-                        self.services_collection.update_one(service_to_update, status_new_value)
-                        print(self.services_collection.find_one(service_to_update))        # TODO: to delete
+                    # Check status of each service in separate thread.
+                    status_thread = threading.Thread(target=self.check_service_status,
+                                                     args=[service],
+                                                     daemon=True,
+                                                     name=f'service-{uuid4().hex}')
+                    status_thread.start()
             time_interval = time.perf_counter() - timer_start
-            print(time_interval)
             if time_interval > self.timer:
                 continue
             time.sleep(self.timer - time_interval)
+            # print([thread.name for thread in threading.enumerate()])      # TODO" to delete
+
+    def check_service_status(self, service):
+        """
+        Checks the status of a particular service.
+        """
+        # TODO: check ip and continue if exception
+        host = service['host']
+        port = service['port']
+        service_name = service['name']
+        service_to_update = {'name': service_name}  # TODO: validate unique service name
+        logging.info(f'Started checking the "{service_name}" service')
+        if service['proto'] == 'tcp':
+            # Set timeout 1s (w 1)
+            response = os.system(f'nc -z -w 1 {host} {port}')
+        else:
+            response = os.system(f'nc -zu {host} {port}')
+        if response == 0:
+            # Mark service as responded
+            response_new_value = {'$set': {'last_responded': datetime.utcnow()}}
+            self.services_collection.update_one(service_to_update, response_new_value)
+            service['last_responded'] = datetime.utcnow()
+            service_status = True
+        else:
+            service_status = False
+        logging.info(f'The "{service_name}" service is {"UP" if service_status else "DOWN"}')
+        # Update service in db only if 'status' is changed
+        if service_status != service['service_up']:
+            status_new_value = {'$set': {'service_up': service_status}}
+            print(self.services_collection.find_one(service_to_update))  # TODO: to delete
+            # Update service status (update db document)
+            self.services_collection.update_one(service_to_update, status_new_value)
+            print(self.services_collection.find_one(service_to_update))  # TODO: to delete
 
     def get_services(self):
         """
         Fetch all services from db.
         """
         return self.services_collection.find()
-
-    def update_service(self, service):
-        pass
 
     @staticmethod
     def check_ip(host):
@@ -78,7 +92,7 @@ class MonitoringService:
 
 if __name__ == "__main__":
     log_format = '%(asctime)s: %(message)s'
-    logging.basicConfig(format=log_format, level=logging.INFO, datefmt='%H:%M:%S')
+    logging.basicConfig(format=log_format, level=logging.DEBUG)
 
     # TODO: mongo is not reachable
 
